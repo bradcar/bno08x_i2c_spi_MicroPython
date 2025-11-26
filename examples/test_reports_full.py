fcbea1c321db602f62880f392887030698acd161
bradcar <bc@bradcarlile.com>
2025-11-26 12:12:58 -0800

    update test_reports and create test_reports_full.py

--- examples/test_reports_full.py
+++ examples/test_reports_full.py
@@ -1,15 +1,18 @@
-# test_reports.py
+# test_reports_full.py
 #
 # BNO08x MicroPython I2C Test
 #
 # I2C interface: Test common sensor reports:
-# acceleration, magnetic, gryoscope, quaternion, quaternion.euler
+# acceleration.full, magnetic.full, gryo.full, quaternion.full, quaternion.euler_full
+# full reports with accuracy and timestamps
+# notice: with slow report frequency, the report can be from last time at not at this time
 
 from time import sleep
 
 from bno08x import *
 from i2c import BNO08X_I2C
 from machine import I2C, Pin
+from utime import ticks_ms
 
 int_pin = Pin(14, Pin.IN, Pin.PULL_UP)  # BNO sensor (INT)
 reset_pin = Pin(15, Pin.OUT)  # BNO sensor (RST)
@@ -33,17 +36,25 @@ print("\nBNO08x sensors enabled")
 
 while True:
     sleep(.25)
-    accel_x, accel_y, accel_z = bno.acceleration
+
+    print(f"\nsystem timestamp {ticks_ms()=}")
+
+    accel_x, accel_y, accel_z, acc, ts_us = bno.acceleration.full
     print(f"\nAcceleration X: {accel_x:+.3f}  Y: {accel_y:+.3f}  Z: {accel_z:+.3f}  m/s²")
+    print(f"Acceleration: accuracy={acc}, ms_timestamp={int(ts_us / 1000)}")
 
-    mag_x, mag_y, mag_z = bno.magnetic
+    mag_x, mag_y, mag_z, acc, ts_us = bno.magnetic.full
     print(f"Magnetometer X: {mag_x:+.3f}  Y: {mag_y:+.3f}  Z: {mag_z:+.3f}  uT ms")
+    print(f"Magnetometer: accuracy={acc}, ms_timestamp={int(ts_us / 1000)}")
 
-    gyro_x, gyro_y, gyro_z = bno.gyro
+    gyro_x, gyro_y, gyro_z, acc, ts_us = bno.gyro.full
     print(f"Gyroscope    X: {gyro_x:+.3f}  Y: {gyro_y:+.3f}  Z: {gyro_z:+.3f}  rads/s")
+    print(f"Gyroscope: accuracy={acc}, ms_timestamp={int(ts_us / 1000)}")
 
-    quat_i, quat_j, quat_k, quat_real = bno.quaternion
+    quat_i, quat_j, quat_k, quat_real, acc, ts_us = bno.quaternion.full
     print(f"Quaternion   I: {quat_i:+.3f}  J: {quat_j:+.3f}  K: {quat_k:+.3f}  Real: {quat_real:+.3f}")
+    print(f"Quaternion: accuracy={acc}, ms_timestamp={int(ts_us / 1000)}")
 
-    roll, pitch, yaw = bno.quaternion.euler
+    roll, pitch, yaw, acc, ts_us = bno.quaternion.euler_full
     print(f"Euler Angle: Roll {roll:+.3f}°  Pitch: {pitch:+.3f}°  Yaw: {yaw:+.3f}°  degrees")
+    print(f"Euler Angle: accuracy={acc}, ms_timestamp={int(ts_us / 1000)}")
